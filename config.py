b77f6787d11c8d6ca2bdc14e513063d350fa8ecd
ferky01 <korgeov@gmail.com>
2023-05-31 23:27:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 from functools import lru_cache
 
+import json
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -63,3 +66,57 @@ def fetch_counters(hero_name, period):
     else:
         print(f"Ошибка при получении данных: {response.status_code}")
         return []
+
+
+def save_economy_data_to_json(period):
+    url_economy = f"https://www.dotabuff.com/heroes/economy?date={period}"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/58.0.3029.110 Safari/537.36",
+    }
+
+    response_economy = requests.get(url_economy, headers=headers)
+
+    if response_economy.status_code == 200:
+        soup_economy = BeautifulSoup(response_economy.content, "html.parser")
+
+        # Parse economy data
+        table_economy = soup_economy.find("table", class_="sortable")
+        economy_data = []
+        for row in table_economy.find_all("tr")[1:]:
+            cells = row.find_all("td")
+            hero_name = cells[1].text.strip()
+            gold_per_minute = cells[2].text.strip()
+            experience_per_minute = cells[3].text.strip()
+
+            # Add economy data to your economy_data
+            economy_data.append({
+                "hero_name": normalize_hero_name(hero_name),
+                "gold_per_minute": gold_per_minute,
+                "experience_per_minute": experience_per_minute,
+            })
+
+        # Create directory if it doesn't exist
+        if not os.path.exists('data_economy'):
+            os.makedirs('data_economy')
+
+        # Save data to json file
+        with open(f'data_economy/economy_data_{period}.json', 'w') as f:
+            json.dump(economy_data, f)
+    else:
+        print(f"Ошибка при получении данных: {response_economy.status_code}")
+
+
+def load_or_fetch_economy_data(period):
+    filename = f'data_economy/economy_data_{period}.json'
+
+    # Check if file exists
+    if not os.path.isfile(filename):
+        # If file doesn't exist, fetch and save data
+        save_economy_data_to_json(period)
+
+    # Load data from json file
+    with open(filename, 'r') as f:
+        economy_data = json.load(f)
+    return economy_data
